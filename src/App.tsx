fd604acf30ac834ea61b52b972091357c763f470
aryelzx <arielcearamor102@gmail.com>
2023-11-15 22:46:55 -0300

    (File) chore(path): changing route path

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
     <Router>
       <GlobalStyle />
       <Routes>
-        <Route path="*" element={<Redirect />} />
+        <Route path="/" element={<Redirect />} />
         <Route path="users/:username" element={<Home />} />
       </Routes>
     </Router>
